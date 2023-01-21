5eb8947541c459d607822bf748ab37de694c592f
Vinayak Malviya <vinayak.malviya@somaiya.edu>
2023-01-22 02:35:58 +0530

    :construction: wip all modals

--- js/index.js
+++ js/index.js
@@ -2,11 +2,21 @@ import { setupModal, openModal } from "./helpers/modalHandler";
 import { setupFab } from "./helpers/fabHandler";
 
 // Setting up modals
+setupModal("occupy-room-modal");
 setupModal("add-room-modal");
+setupModal("edit-room-modal");
 
-document.getElementById("test-modal").addEventListener("click", (e) => {
-  openModal(e.target, "add-room-modal");
+document.getElementById("occupy-test").addEventListener("click", (e) => {
+  openModal(e.target, "occupy-room-modal");
 });
 
 // Setting up fab
 setupFab("main-fab");
+
+document.getElementById("add-room-button").addEventListener("click", (e) => {
+  openModal(e.target, "add-room-modal");
+});
+
+document.getElementById("edit-room-button").addEventListener("click", (e) => {
+  openModal(e.target, "edit-room-modal");
+});
